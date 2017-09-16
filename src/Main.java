899628113fbfccde45ceb4ddfacdb9e6124a26e5
TejaswiniYaramada <32006913+TejaswiniYaramada@users.noreply.github.com>
2017-09-16 13:00:12 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student s =new Student(3,"Tcsdghcs",null,87);
+		s.getId();
+		s.setId(3);
+		//s.setFullName(fullName);
+		s.getAvgMark();
+		
+		
+		
+		
+	}
+
+}
